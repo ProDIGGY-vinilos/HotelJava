c5e44b11dd88dabdd6f692b9618b83c8a077d5ef
Alejandro <alemail@usa.net>
2022-08-31 12:23:22 -0300

    Clases Clintes/Facturas/Reservas

--- Hotel Java/src/entities/Clientes.java
+++ Hotel Java/src/entities/Clientes.java
@@ -1,5 +1,37 @@
 package entities;
 
 public class Clientes {
-
+	private int id_cliente;
+	private int id_usurario;
+	private String nacionalidad;
+	private String pasaporte;
+	
+	
+	public int getId_cliente() {
+		return id_cliente;
+	}
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+	public int getId_usurario() {
+		return id_usurario;
+	}
+	public void setId_usurario(int id_usurario) {
+		this.id_usurario = id_usurario;
+	}
+	public String getNacionalidad() {
+		return nacionalidad;
+	}
+	public void setNacionalidad(String nacionalidad) {
+		this.nacionalidad = nacionalidad;
+	}
+	public String getPasaporate() {
+		return pasaporte;
+	}
+	public void setPasaporate(String pasaporate) {
+		this.pasaporte = pasaporate;
+	}
+	
+	
+	
 }
